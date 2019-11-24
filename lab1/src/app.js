22342e15cfb44f6311edd1198f4152f9ff40e71d
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-11-24 22:27:05 +0300

    рефакторинг.

--- lab1/src/app.js
+++ lab1/src/app.js
@@ -1,41 +1,11 @@
-let FIGURES = [];
-
 function init(canvas_node_id) {
     WEBGL_DRIVER.init(canvas_node_id);
-    
-    FIGURES = [
-        new Figure(
-            new Position(-1.5, 1.0, -7.0),
-            [
-                new Vertex(0.0,  1.0,  0.0),
-                new Vertex(-1.0, -1.0,  0.0),
-                new Vertex(1.0, -1.0,  0.0)
-            ]
-        ),
-        new Figure(
-            new Position(1.5, 0.0, -5.0),
-            [
-                new Vertex(1.0,  1.0,  0.0),
-                new Vertex(-1.0,  1.0,  0.0),
-                new Vertex(1.0, -1.0,  0.0),
-                new Vertex(-1.0, -1.0,  0.0)
-            ]
-        ),
-        new Figure(
-            new Position(0.0, -3.0, -14.0),
-            [
-                new Vertex(1.1,  1.1,  -1.1),
-                new Vertex(-1.1,  1.1,  -1.1),
-                new Vertex(1.1, -1.1,  -1.1),
-                new Vertex(-1.1, -1.1,  -1.1)
-            ]
-        )
-    ];
+    SHAPES_REPOSITORY.init();
 }
 
 function main(canvas_node_id) {
     init(canvas_node_id);
-    FIGURES.forEach((figure) => {
+    SHAPES_REPOSITORY.data.forEach((figure) => {
         figure.draw();
     });
 }
---
+++ lab1/src/models/shapes_repository.js
@@ -0,0 +1,34 @@
+let SHAPES_REPOSITORY = {
+    data: [],
+
+    init: function() {
+        SHAPES_REPOSITORY.data = [
+            new Figure(
+                new Position(-1.5, 1.0, -7.0),
+                [
+                    new Vertex(0.0,  1.0,  0.0),
+                    new Vertex(-1.0, -1.0,  0.0),
+                    new Vertex(1.0, -1.0,  0.0)
+                ]
+            ),
+            new Figure(
+                new Position(1.5, 0.0, -5.0),
+                [
+                    new Vertex(1.0,  1.0,  0.0),
+                    new Vertex(-1.0,  1.0,  0.0),
+                    new Vertex(1.0, -1.0,  0.0),
+                    new Vertex(-1.0, -1.0,  0.0)
+                ]
+            ),
+            new Figure(
+                new Position(0.0, -3.0, -14.0),
+                [
+                    new Vertex(1.1,  1.1,  -1.1),
+                    new Vertex(-1.1,  1.1,  -1.1),
+                    new Vertex(1.1, -1.1,  -1.1),
+                    new Vertex(-1.1, -1.1,  -1.1)
+                ]
+            )
+        ];
+    }
+}
