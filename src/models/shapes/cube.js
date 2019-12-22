f63f2c620858ce203063d983e7069615db086253
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-12-22 20:53:49 +0300

    куб на  основе параллелепипеда

--- src/models/shapes/cube.js
+++ src/models/shapes/cube.js
@@ -1,37 +1,5 @@
 class Cube {
     static build(position, size, angle, color) {
-        return [
-            new Figure(position,
-                [
-                    new Vertex(-size/2,  -size/2,  -size/2, color),
-                    new Vertex(size/2,  -size/2,  -size/2, color),
-                    new Vertex(size/2,  size/2,  -size/2, color),
-                    new Vertex(-size/2,  size/2,  -size/2, color),
-
-                    new Vertex(-size/2,  size/2,  size/2, color),
-                    new Vertex(-size/2,  -size/2,  size/2, color),
-                    new Vertex(size/2,  -size/2,  size/2, color),
-                    new Vertex(size/2,  -size/2,  -size/2, color),
-                ],
-                angle,
-                DRAWING_TYPE.FAN
-            ),
-
-
-            new Figure(position,
-                [
-                    new Vertex(size/2,  size/2,  size/2, color),
-                    new Vertex(size/2,  -size/2,  -size/2, color),
-                    new Vertex(size/2,  size/2,  -size/2, color),
-                    new Vertex(-size/2,  size/2,  -size/2, color),
-                    new Vertex(-size/2,  size/2,  size/2, color),
-                    new Vertex(-size/2,  -size/2,  size/2, color),
-                    new Vertex(size/2,  -size/2,  size/2, color),
-                    new Vertex(size/2,  -size/2,  -size/2, color),
-                ],
-                angle,
-                DRAWING_TYPE.FAN
-            ),
-        ]
+        return Parallelepiped.build(position, size, size, size, angle, color);
     }
 }
