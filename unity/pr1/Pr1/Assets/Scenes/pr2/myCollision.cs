386ce4ee3fd922b86f05d19a0c9f46f80f1a1059
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2020-01-24 00:22:44 +0300

    pr2

---
+++ unity/pr1/Pr1/Assets/Scenes/pr2/myCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision myCollision)
+    {
+    	var cubeRenderer = myCollision.gameObject.GetComponent<Renderer>();
+		cubeRenderer.material.SetColor("_Color", Color.red); 
+    }
+}
