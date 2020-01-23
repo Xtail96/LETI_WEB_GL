91660192977e3342be2adcaeef6df32be76c56ad
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2020-01-24 01:49:50 +0300

    pr3

---
+++ unity/pr1/Pr1/Assets/Scenes/pr3/Ball.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    void OnCollisionEnter(Collision myCollision)
+    {
+		Destroy(myCollision.gameObject);
+	}
+}
---
+++ unity/pr1/Pr1/Assets/Scenes/pr3/Cubes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+	public GameObject cube;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Debug.Log("Start");
+		for (var y = 0; y < 3; y++)
+		{
+			for (var x = -10; x < 10; x++)
+			{
+				for (var z = -10; z < 10; z++)
+				{
+					cube.transform.position = new Vector3(x,y, z);
+					Instantiate(cube, cube.transform.position, Quaternion.identity);
+				}
+			}
+		}
+	}
+}
